94c993631aebd532cd3a6abb4ea9d05c1f1257a7
Rickie Anand <mumbai85@gmail.com>
2024-10-16 01:04:46 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Wordz from './components/Wordz'
+function App() {
+  return (
+    <div className="app">
+      <header className="app-body">
+        Wordz
+        <Wordz />
+      </header>
+    </div>
+  );
+}
+
+export default App;
